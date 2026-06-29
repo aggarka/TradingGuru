848de58fe8827aa990dc63bcb328a7154d105aa9
Kamal Aggarwal <kamauagg@amazon.com>
2026-06-29 12:02:30 -0700

    Initial Commit

---
+++ TradingGuru/TradingGuruApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingGuruApp.swift
+//  TradingGuru
+//
+//  Created by Aggarwal, Kamal on 6/29/26.
+//
+
+import SwiftUI
+
+@main
+struct TradingGuruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
